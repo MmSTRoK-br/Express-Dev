29c6c3432a41a1feadcf559fb4bb6833de3af06e
qualityseg <51680772+matheusmiguel-santos@users.noreply.github.com>
2023-06-19 11:01:16 -0300

    up

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const mercadopago = require('mercadopago');
 
 const app = express();
